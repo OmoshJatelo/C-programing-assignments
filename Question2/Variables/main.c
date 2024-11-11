46cd10e3f70775b4e765ad6471d07f51d9d2d857
SelineOwino <selineowino36@gmail.com>
2024-11-11 16:46:24 +0300

    Variable declaration

---
+++ Question2/Variables/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int adm_number, subject1, subject2, subject3;
+    float average_marks;
+    char surname[20];
+    char choice;
+
+    return 0;
+}
