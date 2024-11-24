3c46d70b46d5dd56820eb0566f06f49968a5794b
digeshiotero <otanaadongo@gmail.com>
2024-11-24 14:44:41 +0300

    Update question3-solution.c

--- Question3/question3-solution.c
+++ Question3/question3-solution.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <mathlib.h>
 
-int quantity;
-float total_price, credit_limit;
-int main(){
+int main()
+{
+    int credit_limit, price, quantity, total_price, n, counter, quantity2;
+    counter = 1;
+    printf("Enter number of clients\n");
+    scanf("%d",&n);
+    while(counter <= n){
 
-do{
+        printf("Enter your credit limit\n");
+        scanf("%d",&credit_limit);
 
-    printf("Enter the quantity:\n");
-    scanf(%f d,&quantity);
-    total_price = price * quantity;
+        printf("Enter price:\n");
+        scanf("%d",&price);
 
-    if(total_price > credit_limit){
-        printf("You cannot purchase goods worth such avalue on credit.\n");
+
+        do{
+
+            printf("Enter the quantity:\n");
+            scanf("%d",&quantity);
+
+            total_price = price * quantity;
+            if(total_price > credit_limit){
+                printf("Sorry you cannot purchase goods worth such a value on credit\n");
+            }
+        }while(total_price > credit_limit);
+            printf("Thank you for purchasing from us\n the total was %d\n", &total_price);
+            counter ++;
+
     }
-}while(total_price > credit_limit){
-    printf("Thank you for purchasing from us\nthe total price is: %d\n", total_price);
-    counter ++;
-
-}
     return 0;
 }
